331bdf00bfee5851f49ce3c087b2b168ae24e341
Leonid <kosmynin.lenya@yandex.ru>
2019-10-04 20:49:11 +0400

    Проверка культурности

--- src/main/java/HomeWork/PoliteTreatment.java
+++ src/main/java/HomeWork/PoliteTreatment.java
@@ -5,10 +5,7 @@ public class PoliteTreatment {
         Scanner sc = new Scanner(System.in);
         System.out.println("insert row");
         String line = sc.nextLine();
-        String line1;
-        line1 = line.replaceAll("Здравствуйте", "*");
-        line1 = line.replaceAll("Здравствуй", "*");
-        if (line1.charAt(0) == '*'){
+        if (line.startsWith("Здравствуйте")){
             System.out.println("Yes");
         } else {
             System.out.println("No");
